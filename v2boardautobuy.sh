59f15701732fd9ff1ceab332c7d22e5c93216334
ciiki <ciiki@ciiki.cf>
2023-12-11 09:27:21 +0800

    update v2board autobuy

--- v2boardautobuy.sh
+++ v2boardautobuy.sh
@@ -3,6 +3,8 @@ int=0
 domain=
 email=
 passwd=
+planid=
+coupon=
 
 authcode=$(curl -s "https://$domain/api/v1/passport/auth/login" --data-raw "email=$email&password=$passwd" --compressed| python3 -c "import sys, json; restr=json.load(sys.stdin); print(restr['data']['auth_data'])") 
 
@@ -11,7 +13,7 @@ echo $authcode
 while(( $int<60 ))
 do
     let "int++"
-    orderno=$(curl -s "https://$domain/api/v1/user/order/save" -H "authorization:${authcode}" --data-raw 'period=month_price&plan_id=1&coupon_code=' --compressed | python3 -c "import sys, json; print(json.load(sys.stdin)['data'])")
+    orderno=$(curl -s "https://$domain/api/v1/user/order/save" -H "authorization:${authcode}" --data-raw "period=month_price&plan_id=$planid&coupon_code=$coupon" --compressed | python3 -c "import sys, json; print(json.load(sys.stdin)['data'])")
     echo $orderno
     curl -s "https://$domain/api/v1/user/order/checkout" -H "authorization:${authcode}" --data-raw "trade_no=${orderno}&method=1" --compressed > /dev/null
 done
